15e47ee93a4c64bd153bf8e4f72b6598fe286302
gilldarts <jaime.palomino97@hotmail.com>
2017-04-03 09:23:31 -0500

    modificaciones

--- config/redirect.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	if (isset($_SESSION['urol'])) {
-		if ($_SESSION['urol'] == 'administrador') {
-			header("location:administrador.php");
-		} else if ($_SESSION['urol'] == 'cliente') {
-			header("location:cliente.php");
-		}
-	}
