4cc51ec16776451388bad0bbb892ee7d29cba25a
carincon93 <carincon93@gmail.com>
2017-04-03 11:40:37 -0500

    camilo

--- config/loguin.php
+++ config/loguin.php
@@ -3,12 +3,13 @@
 	$correo = $_POST['correo'];
 	$clave = $_POST['clave'];
  	$sql = "SELECT nombre, apellido FROM administrador WHERE correo = '$correo' AND clave = '$clave'";
- 	
+
 	$query = mysqli_query($con, $sql);
  	if(mysqli_num_rows($query) > 0){
+ 		session_start();
  		$row = mysqli_fetch_array($query);
  		$_SESSION['nombre'] = $row['nombre']." ".$row['apellido']; 		
- 		header("location: ../administrador/index.php");		
+ 		header("location: ../administrador/index.php");
  	}else {
  		echo "<script>
  			alert('Correo o Contraseña Incorrectos');
